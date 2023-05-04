4170281fa7e7cd84a4b4181a46635151e8f89359
Filipa Borges <filipaborges@outlook.pt>
2023-05-04 15:54:33 +0100

    Add files via upload

---
+++ philo/end_condition.c
@@ -0,0 +1,57 @@
+#include "philo.h"
+
+int	end_simulation(t_data *data)
+{
+	int	flag;
+
+	flag = 0;
+	pthread_mutex_lock(&data->end_mutex);
+	flag = data->is_over;
+	pthread_mutex_unlock(&data->end_mutex);
+	return (flag);
+}
+
+void	change_flag_end(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->end_mutex);
+	philo->data->is_over = 1;
+	pthread_mutex_unlock(&philo->data->end_mutex);
+}
+
+int	has_philosopher_finished(t_philo *philo)
+{
+	int	flag;
+
+	flag = 0;
+	pthread_mutex_lock(&philo->data->end_mutex);
+	flag = philo->data->is_over;
+	pthread_mutex_unlock(&philo->data->end_mutex);
+	if (flag)
+		return (flag);
+	return (0);
+}
+
+int	check_meals(t_philo *philo, int i)
+{
+	pthread_mutex_lock(&philo->data->end_mutex);
+	if (i != philo->to_take)
+	{
+		philo->taken++;
+		pthread_mutex_unlock(&philo->data->end_mutex);
+		return (0);
+	}
+	pthread_mutex_unlock(&philo->data->end_mutex);
+	return (1);
+}
+
+int	meals_done(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->end_mutex);
+	if (philo->taken == philo->to_take)
+	{
+		pthread_mutex_unlock(&philo->data->end_mutex);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->data->end_mutex);
+	return (0);
+}
